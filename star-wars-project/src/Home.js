513f75d8d3dfb3c51d11fa25b95fd59dded43a6b
Lrobert6 <lrobert6@nait.ca>
2025-01-21 23:18:27 -0700

    Added Audio

--- star-wars-project/src/Home.js
+++ star-wars-project/src/Home.js
@@ -1,10 +1,17 @@
+import { useEffect, useState } from 'react';
 import React from "react"
 import {useNavigate} from 'react-router-dom';
+import useSound from 'use-sound';
+import Naboo from './img/naboo.png';
+import HoverSoundEffect from'./sfx/hover-sound-effect.mp3';
 
 const Home = () => {
+    const soundUrl = HoverSoundEffect;
+    const [play] = useSound(soundUrl);
+
     return (
         <div>
-            <h1>Home</h1>
+            <img src={Naboo} onMouseEnter={play} className='planet-image' style={{width: '15%'}} alt='Naboo'/>
         </div>
     )
 }
